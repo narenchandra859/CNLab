e05d21ccd59e6cb5dfa9a63aa2fe89d343edb6a9
Naren Chandra <44522774+narenchandra859@users.noreply.github.com>
2019-12-23 18:12:00 +0530

    Update HammingCode.cpp

--- HammingCode.cpp
+++ HammingCode.cpp
@@ -1,32 +1,32 @@
-#include<iostream>
-#include<cstring>
+#include "iostream"
+#include "cstring"
 using namespace std;
-int main(){ 
-	string dataWord, codeWord="0000000",recWord;
-	int posBits[3];
-	cout<<"\nEnter 4 bit data word: ";
-	cin>>dataWord;
-	codeWord[4] = dataWord[3];
-	codeWord[2] = dataWord[2];
-	codeWord[1] = dataWord[1];
-	codeWord[0] = dataWord[0];
-	// r1 = r1 ^ dataPos(1,3,5,7)[3 bit table] --> r1 = r1^(p3==dataWord[0])^(p5==dataWord[1])^(p7==dataWord[3])
-	codeWord[6] = dataWord[3]^dataWord[2]^dataWord[0];
-	codeWord[5] = dataWord[3]^dataWord[1]^dataWord[0];
-	codeWord[3] = dataWord[2]^dataWord[1]^dataWord[0];
-	cout<<"\nRedundant bits are: "<<codeWord[3]<<" "<<codeWord[5]<<" "<<codeWord[6];
-	cout<<"\nEnter recieved data(7 bits): ";
-	cin>>recWord;
-	posBits[0] = recWord[6]^recWord[4]^recWord[2]^recWord[0];
-	posBits[1] = recWord[5]^recWord[4]^recWord[1]^recWord[0];
-	posBits[2] = recWord[3]^recWord[2]^recWord[1]^recWord[0];
-	if((posBits[0]==0)&&(posBits[1]==0)&&(posBits[2]==0)) 
-		cout<<"\nNo error during transmission.\n";
-	else{
-		int errPos = (1*posBits[0])+(2*posBits[1])+(4*posBits[2]);
-		cout<<"\nError in transmission at bit: "<<errPos;
-		cout<<"\nReal codeword: "<<codeWord;
-		cout<<"\nRecieved codeword: "<<recWord<<endl;
+int main() {
+	string dW, cW="0000000", rW;
+	int pB[3];
+	cout<<"\nEnter the dataword (4 bits): ";
+	cin>>dW;
+	cW[4]=dW[3];
+	cW[2]=dW[2];
+	cW[1]=dW[1];
+	cW[0]=dW[0];
+	cW[6] = dW[3]^dW[2]^dW[0];
+	cW[5] = dW[3]^dW[1]^dW[0];
+	cW[3] = dW[2]^dW[1]^dW[0];
+	cout<<"\nRedundant bits = "<<cW[3]<<" "<<cW[5]<<" "<<cW[6];
+	cout<<"\nEnter recieved data: ";
+	cin>>rW;
+	pB[0] = rW[6]^rW[4]^rW[2]^rW[0];
+	pB[1] = rW[5]^rW[4]^rW[1]^rW[0];
+	pB[2] = rW[3]^rW[2]^rW[1]^rW[0];
+	if((pB[0]==0)&&(pB[1]==0)&&(pB[2]==0))
+		cout<<"\nNo error in transmission.\n";
+	else {
+		cout<<"\nError during transmission.";
+		int err = (1*pB[0])+(2*pB[1])+(4*pB[2]);
+		cout<<"\nError at position "<<7-err;
+		cout<<"\nExpected codeword:\t"<<cW;
+		cout<<"\nRecieved codeword:\t"<<rW<<endl;
 	}
 	return 0;
 }
