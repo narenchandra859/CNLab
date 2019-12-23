7fa7d47fa026067d62dbc57b5e63ffbebc485dec
Naren Chandra <44522774+narenchandra859@users.noreply.github.com>
2019-12-23 18:10:22 +0530

    Update CRC16bit.cpp

--- CRC16bit.cpp
+++ CRC16bit.cpp
@@ -1,56 +1,53 @@
-#include<iostream>
-#include<cstring>
+#include "iostream"
+#include "cstring"
 using namespace std;
-string crc(string gP, string dataWord, int k) {
-  int pos = 17;
-  char z[3] = "01";
-  string zero = "0", one = "1", rem, newrem;
-  for(int i = 0; i<gP.length()-1; i++) 
-    rem.append((gP[i+1]==dataWord[i+1])?zero:one);
-  rem.append((dataWord[pos++]==z[1])?one:zero);
-  for(int j = 0; j<k-1; j++) {
-    newrem = rem;
-    if(rem[0] == z[1]) {
-      rem = "";
-      for(int i = 0; i<gP.length()-1; i++)
-        rem.append((gP[i+1]==newrem[i+1])?zero:one);
-    }
-    else
-      rem = newrem.substr(1,16);
-    if(j!=k-2)rem.append((dataWord[pos++]==z[1])?one:zero);
-  }
-  return rem;
+string crc(string gP, string d, int k) {
+	int pos = 17;
+	string rem=d, newrem;
+	for(int i = 0; i<k; i++) {
+		newrem = rem;
+		rem="";
+		if(newrem[0]=='1') {
+			for(int j = 1; j<17; j++) 
+				rem.append(newrem[j]==gP[j]?"0":"1");
+		}
+		else 
+			rem = newrem.substr(1,16);
+		if(i!=k-1)
+			rem.append(d[pos++]=='0'?"0":"1");
+	}
+	return rem;
 }
 int main() {
-  string gP = "10001000000100001", codeWord, dataWord, zero = "0", one = "1";
-  cout<<"\nEnter the dataword : ";
-  char z[3]="01";
-  cin>>dataWord;
-  int k = dataWord.length();
-  codeWord = dataWord;
-  for(int i = 0; i<16; i++) 
-    dataWord.append("0");
-  cout<<"\nNew dataword = "<<dataWord;
-  string rem = crc(gP, dataWord, k);
-  cout<<"\nCRC rem = "<<rem;
-  codeWord.append(rem);
-  cout<<"\nNew codeword = "<<codeWord;
-  cout<<"\nCheck error? Y/N: ";
-  cin>>z[2];
-  if(z[2]=='Y') {
-    cout<<"\nEnter position of error: ";
-    int p;
-    cin>>p;
-    codeWord[p] = (codeWord[p]==z[0])?z[1]:z[0];
-    cout<<"\nSending codeword "<<codeWord;
-    rem = crc(gP, codeWord, k);
-    cout<<"\nRemainder = "<<r;
-    cout<<"\nRemainder not zero! Error!";
-  }
-  else {
-    rem = crc(gP, codeWord, k);
-    cout<<"\nSending codeword "<<codeWord;
-    cout<<"\nRemainder = "<<r;
-    cout<<"\nRemainder zero, no error!";
-  }
+	string gP="10001000000100001", codeWord, dataWord;
+	cout<<"\nEnter the data word: ";
+	cin>>dataWord;
+	int k = dataWord.length();
+	codeWord = dataWord;
+	for(int i = 0; i<16; i++)
+		dataWord.append("0");
+	cout<<"\nNew data word = "<<dataWord;
+	string rem = crc(gP, dataWord, k);
+	cout<<"\nCRC remainder = "<<rem;
+	codeWord.append(rem);
+	cout<<"\nNew codeword = "<<codeWord<<endl;
+	cout<<"1.Check Error\n2.Exit:";
+	int p, c;
+	cin>>c;
+	if(c==1) {
+		cout<<"\nEnter position of error: ";
+		cin>>p;
+		codeWord[p]=(codeWord[p]=='0'?'1':'0');
+		cout<<"\nSending codeword: "<<codeWord;
+		rem = crc(gP, codeWord, k);
+		cout<<"\nRemainder: "<<rem;
+		cout<<"\nNot zero, error!";
+	}
+	else {
+		cout<<"\nSending codeword: "<<codeWord;
+		rem = crc(gP, codeWord, k);
+		cout<<"\nRemainder: "<<rem;
+		cout<<"\nZero, no error.";
+	}
+	return 0;
 }
